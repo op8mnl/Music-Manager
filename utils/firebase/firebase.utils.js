2be5a439f8f534204f87fefe3cf64fca6695a4fe
Kerem Hatiboglu <khatibog@uwo.ca>
2022-11-26 14:07:51 -0500

    finished the utils for the firebase added the signin -signup, signout and can add users to firebase

--- utils/firebase/firebase.utils.js
+++ utils/firebase/firebase.utils.js
@@ -11,17 +11,69 @@ import {
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDzPmPf5FocGqIZAHjd4Rzd1BQt5EzkB-0",
-  authDomain: "se3316-lab4-db.firebaseapp.com",
-  projectId: "se3316-lab4-db",
-  storageBucket: "se3316-lab4-db.appspot.com",
-  messagingSenderId: "1005909444403",
-  appId: "1:1005909444403:web:31672e2b404e5b5cca6046",
+  apiKey: "AIzaSyDfpkC1oknEVR51YhLDoDswIvRxsgt5B30",
+  authDomain: "se3316-lab4-e979f.firebaseapp.com",
+  projectId: "se3316-lab4-e979f",
+  storageBucket: "se3316-lab4-e979f.appspot.com",
+  messagingSenderId: "161946395327",
+  appId: "1:161946395327:web:94cb7824ff35e02924dc0a",
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export const fireDb = getFirestore();
 
 export const auth = getAuth();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(fireDb, "users", userAuth.uid);
+  //console.log(userDocRef);
+
+  //taking the snapshot of user to compare later (like a data)
+  const userSnapshot = await getDoc(userDocRef);
+  //console.log(userSnapshot);
+  //console.log(userSnapshot.exists());
+
+  //adding the user to the firebase so we can track who is signing up
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user in the databse", error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+export const onAuthStateChanged = (callback) =>
+  onAuthStateChanged(auth, callback);

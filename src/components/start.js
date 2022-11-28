885d2a5b53ea6715b12e8aed376eb3b026817e65
Felix Zheng <zheng.felix@outlook.com>
2022-11-28 17:32:45 -0500

    start page

--- src/components/start.js
+++ src/components/start.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 function Start(){
     return(
@@ -10,8 +10,17 @@ function Start(){
                         SE3316 Lab 4 
                     </div>
                 </div>
-                <div class="box"> 
-                
+                <div class="box">
+                    <div class="start-box">
+                        <div style={{order:"1",width:"100%"}}>
+                            <span style = {{textAlign:"center"}}>
+                                This interactive web app allows you to create, view, and share various playlists created by users around the world!
+                            </span>
+                        </div>
+                        <div style={{order:"2",width:"100%"}}>
+                            <a href="/auth"><input class="button"id="login"type="button"value="Login"/></a>
+                        </div>
+                    </div>
                 </div>
             </body>
         </>

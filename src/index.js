4b9db0457cd00f5299671f4d8c4cbb4c9a3f439e
Shayaan <skhatri7@uwo.ca>
2022-11-28 13:24:03 -0500

    Added routing between react webpages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import AUP from './src/AUP'
+import PrivacyPolicy from './src/privacypolicy'
+import DMCAPolicy from './src/DMCAPolicy'
+
+const Routing = () => {
+  return(
+    <Router>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/privacypolicy" component={PrivacyPolicy} />
+        <Route path="/aup" component={AUP} />
+        <Route path="/dmcapolicy" component={DMCAPolicy} />
+      </Switch>
+      <Footer/>
+    </Router>
+  )
+}
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );

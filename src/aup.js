1c6a5cd99ebbdfb66daa59ba7154e59d3757017a
Shayaan <skhatri7@uwo.ca>
2022-11-28 15:22:28 -0500

    styled polict pages

--- src/aup.js
+++ src/aup.js
@@ -1,60 +1,64 @@
 import React from "react";
 
-
+const style = {
+    color: "black"
+}
 
 function AUP(){
     return(
     <>
-       <h1>Acceptable Use Policy</h1>
-<p>Last updated: November 28, 2022</p>
-<h2><strong>AUP for Corporate Internet Systems</strong></h2>
+    <div>
+       <h1 style={style}>Acceptable Use Policy</h1>
+<p style={style}>Last updated: November 28, 2022</p>
+<h2 style = {style}><strong>AUP for Corporate Internet Systems</strong></h2>
 <ul>
     <li>
-        <p>Certain employees may be provided with access to the Internet to assist them in performing their jobs. The Internet can be a valuable source of information and research. Use of the Internet, however, must be tempered with common sense and good judgment.</p>
+        <p style={style}>Certain employees may be provided with access to the Internet to assist them in performing their jobs. The Internet can be a valuable source of information and research. Use of the Internet, however, must be tempered with common sense and good judgment.</p>
     </li>
     <li>
-        <p>If you abuse your right to use the Internet, it will be taken away from you. In addition, you may be subject to disciplinary action, including possible termination, and civil and criminal liability.</p>
+        <p style={style}>If you abuse your right to use the Internet, it will be taken away from you. In addition, you may be subject to disciplinary action, including possible termination, and civil and criminal liability.</p>
     </li>
     <li>
-        <p>Disclaimer of liability for use of Internet. SE3316 Lab 4 is not responsible for material viewed or downloaded by users from the Internet. The Internet is a worldwide network of computers that contains millions of pages of information. Users are cautioned that many of these pages include offensive, sexually explicit, and inappropriate material. In general, it is difficult to avoid at least some contact with this material while using the Internet. In addition, having an e-mail address on the Internet may lead to receipt of unsolicited e-mail containing offensive content. Users accessing the Internet do so at their own risk.</p>
+        <p style={style}>Disclaimer of liability for use of Internet. SE3316 Lab 4 is not responsible for material viewed or downloaded by users from the Internet. The Internet is a worldwide network of computers that contains millions of pages of information. Users are cautioned that many of these pages include offensive, sexually explicit, and inappropriate material. In general, it is difficult to avoid at least some contact with this material while using the Internet. In addition, having an e-mail address on the Internet may lead to receipt of unsolicited e-mail containing offensive content. Users accessing the Internet do so at their own risk.</p>
     </li>
     <li>
-        <p>Duty not to waste computer resources. Employees must not deliberately perform acts that waste computer resources or unfairly monopolize resources to the exclusion of others. These acts include, but are not limited to, sending mass mailings or chain letters, spending excessive amounts of time on the Internet, playing games, engaging in online chat groups, printing multiple copies of documents, or otherwise creating unnecessary network traffic. Because audio, video and picture files require significant storage space, files of this or any other sort may not be downloaded unless they are business-related.</p>
+        <p style={style}>Duty not to waste computer resources. Employees must not deliberately perform acts that waste computer resources or unfairly monopolize resources to the exclusion of others. These acts include, but are not limited to, sending mass mailings or chain letters, spending excessive amounts of time on the Internet, playing games, engaging in online chat groups, printing multiple copies of documents, or otherwise creating unnecessary network traffic. Because audio, video and picture files require significant storage space, files of this or any other sort may not be downloaded unless they are business-related.</p>
     </li>
     <li>
-        <p>No expectation of privacy. The computers and computer accounts given to employees are to assist them in performance of their jobs. Employees should not have an expectation of privacy in anything they create, store, send, or receive on the computer system. The computer system belongs to SE3316 Lab 4 and may only be used for business purposes.</p>
+        <p style={style}>No expectation of privacy. The computers and computer accounts given to employees are to assist them in performance of their jobs. Employees should not have an expectation of privacy in anything they create, store, send, or receive on the computer system. The computer system belongs to SE3316 Lab 4 and may only be used for business purposes.</p>
     </li>
     <li>
-        <p>Monitoring computer usage. SE3316 Lab 4 has the right, but not the duty, to monitor any and all of the aspects of its computer system, including, but not limited to, monitoring sites visited by employees on the Internet, monitoring chat groups and news groups, reviewing material downloaded or uploaded by users to the Internet, and reviewing e-mail sent and received by users.</p>
+        <p style={style}>Monitoring computer usage. SE3316 Lab 4 has the right, but not the duty, to monitor any and all of the aspects of its computer system, including, but not limited to, monitoring sites visited by employees on the Internet, monitoring chat groups and news groups, reviewing material downloaded or uploaded by users to the Internet, and reviewing e-mail sent and received by users.</p>
     </li>
     <li>
-        <p>Blocking of inappropriate content. SE3316 Lab 4 may use software to identify inappropriate or sexually explicit Internet sites. Such sites may be blocked from access by company networks. In the event you nonetheless encounter inappropriate or sexually explicit material while browsing on the Internet, immediately disconnect from the site, regardless of whether the site was subject to company blocking software.</p>
+        <p style={style}>Blocking of inappropriate content. SE3316 Lab 4 may use software to identify inappropriate or sexually explicit Internet sites. Such sites may be blocked from access by company networks. In the event you nonetheless encounter inappropriate or sexually explicit material while browsing on the Internet, immediately disconnect from the site, regardless of whether the site was subject to company blocking software.</p>
     </li>
     <li>
-        <p>Prohibited activities. Material that is fraudulent, harassing, embarrassing, sexually explicit, profane, obscene, intimidating, defamatory, or otherwise unlawful, inappropriate, offensive (including offensive material concerning sex, race, color, national origin, religion, age, disability, or other characteristic protected by law), or violates SE3316 Lab 4's equal employment opportunity policy and its policies against sexual or other harassment may not be downloaded from the Internet or displayed or stored in SE3316 Lab 4's computers. Employees encountering or receiving this kind of material should immediately report the incident to their supervisors. SE3316 Lab 4's equal employment opportunity policy and its policies against sexual or other harassment apply fully to the use of the Internet and any violation of those policies is grounds for discipline up to and including discharge.</p>
+        <p style={style}>Prohibited activities. Material that is fraudulent, harassing, embarrassing, sexually explicit, profane, obscene, intimidating, defamatory, or otherwise unlawful, inappropriate, offensive (including offensive material concerning sex, race, color, national origin, religion, age, disability, or other characteristic protected by law), or violates SE3316 Lab 4's equal employment opportunity policy and its policies against sexual or other harassment may not be downloaded from the Internet or displayed or stored in SE3316 Lab 4's computers. Employees encountering or receiving this kind of material should immediately report the incident to their supervisors. SE3316 Lab 4's equal employment opportunity policy and its policies against sexual or other harassment apply fully to the use of the Internet and any violation of those policies is grounds for discipline up to and including discharge.</p>
     </li>
     <li>
-        <p>Games and entertainment software. Employees may not use the company's Internet connection to download games or other entertainment software, including wallpaper and screen savers, or to play games over the Internet.</p>
+        <p style={style}>Games and entertainment software. Employees may not use the company's Internet connection to download games or other entertainment software, including wallpaper and screen savers, or to play games over the Internet.</p>
     </li>
     <li>
-        <p>Illegal copying. Employees may not illegally copy material protected under copyright law or make that material available to others for copying. You are responsible for complying with copyright law and applicable licenses that may apply to software, files, graphics, documents, messages, and other material you wish to download or copy. You may not agree to a license or download any material for which a registration fee is charged without first obtaining the express written permission of SE3316 Lab 4 Management.</p>
+        <p style={style}>Illegal copying. Employees may not illegally copy material protected under copyright law or make that material available to others for copying. You are responsible for complying with copyright law and applicable licenses that may apply to software, files, graphics, documents, messages, and other material you wish to download or copy. You may not agree to a license or download any material for which a registration fee is charged without first obtaining the express written permission of SE3316 Lab 4 Management.</p>
     </li>
     <li>
-        <p>Accessing the Internet. To ensure security and to avoid the spread of viruses, employees accessing the Internet through a computer attached to SE3316 Lab 4's network must do so through an approved Internet firewall. Accessing the Internet directly by modem is strictly prohibited unless the computer you are using is not connected to SE3316 Lab 4's network.</p>
+        <p style={style}>Accessing the Internet. To ensure security and to avoid the spread of viruses, employees accessing the Internet through a computer attached to SE3316 Lab 4's network must do so through an approved Internet firewall. Accessing the Internet directly by modem is strictly prohibited unless the computer you are using is not connected to SE3316 Lab 4's network.</p>
     </li>
     <li>
-        <p>Virus detection. Files obtained from sources outside SE3316 Lab 4, including disks brought from home; files downloaded from the Internet, newgroups, bulletin boards, or other online services; files attached to e-mail; and files provided by customers or vendors may contain dangerous computer viruses that may damage SE3316 Lab 4's computer network. Employees should never download files from the Internet, accept e-mail attachments from outsiders, or use disks from non-company sources, without first scanning the material with company-approved virus checking software. If you suspect that a virus has been introduced into SE3316 Lab 4's network, notify your supervisor immediately.</p>
+        <p style={style}>Virus detection. Files obtained from sources outside SE3316 Lab 4, including disks brought from home; files downloaded from the Internet, newgroups, bulletin boards, or other online services; files attached to e-mail; and files provided by customers or vendors may contain dangerous computer viruses that may damage SE3316 Lab 4's computer network. Employees should never download files from the Internet, accept e-mail attachments from outsiders, or use disks from non-company sources, without first scanning the material with company-approved virus checking software. If you suspect that a virus has been introduced into SE3316 Lab 4's network, notify your supervisor immediately.</p>
     </li>
     <li>
-        <p>Amendments and revisions. This policy may be amended or revised from time to time as the need arises. Users will be provided with copies of all amendments and revisions.</p>
+        <p style={style}>Amendments and revisions. This policy may be amended or revised from time to time as the need arises. Users will be provided with copies of all amendments and revisions.</p>
     </li>
     <li>
-        <p>Violations of this policy will be taken seriously and may result in disciplinary action, including possible termination, and civil and criminal liability.</p>
+        <p style={style}>Violations of this policy will be taken seriously and may result in disciplinary action, including possible termination, and civil and criminal liability.</p>
     </li>
     <li>
-        <p>Use of the Internet via SE3316 Lab 4's computer system constitutes consent by the user to all of the terms and conditions of this policy.</p>
+        <p style={style}>Use of the Internet via SE3316 Lab 4's computer system constitutes consent by the user to all of the terms and conditions of this policy.</p>
     </li>
 </ul> 
+</div>
     </>
 )
 }
